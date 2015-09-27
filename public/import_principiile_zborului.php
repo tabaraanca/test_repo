d3d697f51c11a69b095d2bca3f9fa180b5737c23
Dan Dumitriu <dan.dumitriu@sfappworks.com>
2015-09-27 18:34:13 +0300

    import script

---
+++ public/import_principiile_zborului.php
@@ -0,0 +1,566 @@
+<?php
+$str = "1. Finetea aerodinamica este:
+a. raportul dintre Cx si Cz
+b. raportul dintre vectorul viteza si greutate
+c. descrie calitatea suprafetei portante
+
+2. Finetea aerodinamica este influentata de :
+a. lungimea suspantelor
+b. clasa de omologare a parapantei
+c. directia vantului
+
+3. Forta portanta se genereaza:
+a. 2/3 pe extrados
+b. 2/3 pe intrados
+c. ½ pe extrados si ½ pe intrados
+
+4. Viteza minima de infundare este influentata de :
+a. viteza de zbor
+b. lungimea suspantelor
+c. directia si viteza vantului
+
+5. Unghiul de incidenta este dat de :
+a. coarda profilului si directia curentului  de aer relativ
+b. extradosul aripii si orizontala
+c. coarda profilului si orizontala
+
+6. Cand unghiul de incidenta creste:
+a. creste portanta, scade viteza
+b. creste portanta, creste viteza
+c. scade portanta, scade viteza
+
+7. Desprinderea stratului limita apare cand:
+a. creste unghiul de incidenta pana la unghi critic
+b. scade unghiul de incidenta la o valoare inferioara unghiului critic
+c. se elibereaza comenzile
+
+8. Forta portanta este influentata de :
+a. unghiul de incidenta
+b. viteza vantului
+c. directia vantului
+
+
+9. Unghiul de atac critic duce la:
+a. desprinderea stratului limita
+b. cresterea portantei la nivel maxim
+c. lipirea fileurilor de aer de extrados
+
+10. Centrul de presiune este:
+a. mobil sau fix functie de tipul profilului aerodinamic
+b. fix.
+c. mobil in toate cazurile
+
+11. Angajarea apare :
+a. la viteza maxima de zbor
+b. la atingerea vitezei minime de infundare
+c. la atingerea unghiului de incidenta critic
+
+12. Dupa legea lui Bernoulli:
+a. cand viteza creste, scade presiunea
+b. cand viteza creste, creste si presiunea
+c. nu are legatura cu viteza de curgere a fluidelor
+
+13. Din polara putem deduce:
+a. viteza minima de infundare
+b. greutatea maxima de zbor
+c. unghiul maxim de inclinare in zbor normal
+
+14. Alungirea aripii este:
+a. raportul dintre coarda medie si anvergura
+ b.raportul dintre profunzimea maxima a aripii si anvergura
+c. raportul dintre grosimea maxima relativa a profilului si anvergura
+
+15. Rezistenta indusa este data de :
+a. turbioanele de la capetele aripii
+b. rezistenta la inaintare a intregii aripii plus cea a suspantelor
+c. rezistenta la inaintare generata de voalura
+
+16. In cazul aripii eliptice, la aripa fara torsiune geometrica, stratul limita se desprinde incepand cu:
+a. capetele de aripa
+b. zona centrala a aripii
+c. paralel cu bordul de atac, in acelasi timp de-a lungul anvergurii
+
+17. Aripa de parapanta prezinta torsiune geometrica:
+a. da
+b. nu
+c. torsiunea geometrica nu are legatura cu mecanica zborului
+
+18. Coarda profilului defineste:
+a. linia ce uneste punctele extreme ale profilului, din zona bordului de atac si din zona bordului de fuga
+b. linia ce uneste bordul de fuga cu directia de curgere a fileurilor de aer
+c. curbura liniei extradosului
+
+19. Care dintre urmatoarele afirmatii este adevarata:
+a. la cresterea unghiului de incidenta scade viteza
+b. la cresterea unghiului de incidenta scade portanta
+c. la cresterea unghiului creste viteza
+
+20. Care dintre urmatoarele afirmatii este adevarata:
+a. rezistenta indusa este influentata de inaltimea de zbor
+b. rezistenta indusa are legatura cu rezistenta la rupere a suspantelor
+c. rezistenta indusa nu are lagatura cu zborul
+
+21. Care dintre urmatoarele afirmatii este adevarata:
+a. rezistenta la inaintare este direct proportionala cu patratul vitezei
+b. rezistenta la inaintare este invers proportinala cu patratul vitezei
+c. rezistenta la inaintare este direct proportionala doar cu aria sectiunii
+
+
+22. Teoria care sta la baza zborului cu parapanta face parte din:
+a. domeniul fluidelor compresibile
+b. domeniul fluidelor incompresibile
+c. nu are lagatura cu compresibilitatea
+
+23. Care dintre urmatoarele nu face parte din domeniul aerodinamicii:
+a. rezistenta ohmica
+b. rezistenta indusa
+c. rezistenta parazita
+
+24. Viteza de curgere a fileurilor de aer pe un profil aerodinamic este:
+a. mai mare pe extrados decat pe intrados
+b. mai mare pe intrados decat pe extrados
+c. egala intrados cu extrados
+
+25. Turbioanele terminale responsabile pentru rezistenta indusa apar:
+a. pe capetele aripii
+b. in spatele aripii (siaj)
+c. in spate si sub aripa
+
+26. Centrul de presiune este:
+a. fix sau mobil functie de tipul profilului
+b. mobil
+c. nu exista aceasta notiune in aviatie
+
+27. Centrul de presiune se regaseste intotdeauna  la nivelul:
+a. in jumatatea dinainte a aripii
+b. bordului de fuga in zona centrala a aripii
+c. bordului de atac al aripii
+
+28. In care din urmatoarele cazuri viteza de angajare creste:
+a. creste incarcarea alara
+b. scade viteza de zbor fata de sol
+c. micsoram numarul de suspante
+
+
+29. La atingerea unghiului de atac critic:
+a. portanta scade brusc, iar rezistenta la inaintare creste brusc
+b. portanta creste proportional cu scaderea vitezei
+c. portanta creste proportional cu viteza
+
+30. Desprinderea stratului limita apare:
+a. la atingerea unghiului de atac critic
+b. la atingerea vitezei minime de zbor
+c. la atingerea vitezei maxime de zbor
+
+31. Conform teoremei lui Bernoulli atunci cand sectiunea unui tub se micsoreaza:
+a. viteza de curgere creste iar presiunea statica scade
+b. viteza de curgere creste iar presiunea statica creste
+c. viteza de curgere scade iar presiunea statica creste
+
+32. Din polara vitezelor putem deduce:
+a. viteza minima de infundare, viteza la finete maxima, viteza maxima de zbor
+b. viteza maxima in viraj
+c. viteza de zbor in functie de altitudine
+
+33. Infundarea minima se obtine :
+a. la o viteza inferioara vitezei la finete maxima
+ b. la o viteza egala cu viteza la finete maxima
+c. la o viteza mai mare decat viteza la finete maxima
+
+34. Finetea parapantei reprezinta:
+a. unghiul de panta in zbor exprimat printr-un raport
+b. calitatea finisajului extradosului si intradosului
+c. coeficientul de frecare cu aerul al intregului ansamblu: voalura, suspante, pilot
+
+35. Finetea aerodinamica a parapantei este influentata de :
+a. valoarea rezistentei la inaintare
+b.valoarea vitezei maxime de zbor
+c. valoarea vitezei de angajare
+
+36. Care dintre urmatoarele afirmatii este adevarata:
+a. presiunea dinamica este influentata de viteza
+b. presiunea statica este intotdeauna egala cu presiunea dinamica
+c. presiunea dinamica este influentata de altitudine
+
+37. Pentru a imbunatati finetea unei parapante putem sa:
+a. folosim o seleta profilata (cocon)
+b. marim suprafata portanta
+c. sa micsoram suprafata portanta
+
+38. In zbor planat stabilizat forta portanta este:
+a. mai mica decat greutatea
+b. mai mare decat greutatea
+c. egala cu greutatea
+
+39. Angajarea in limita de viteza apare ca urmare:
+a. ruperii fileurilor de aer de pe extrados
+b. ruperii fileurilor de aer de pe intrados
+c. zborului la viteza minima
+
+40. In timpul procedurii de decolare unghiul de atac este:
+a. variabil
+b. fix
+c. variaza doar daca se actioneaza acceleratorul
+
+41. Care din urmatoarele au drept consecinta cresterea finetei unei parapante:
+a. micsorarea numarului de suspante
+b. scaderea incarcarii alare
+c. cresterea greutatii totale
+
+42. Profilul aripii este dat de :
+a. membranele intercheson
+b. proiectia aripii pe un plan orizontal
+c. suprafata aripii desfasurata
+
+43. Viteza minima de infundare este:
+a. mai mica decat viteza la finete maxima
+b. mai mare decat viteza la finete
+c. egala cu viteza la finete
+
+44. Regimurile de zbor ale parapantei sunt:
+a. viteza minima, viteza maxima, viteza accelerata
+b. viteza la infundare minima, viteza la finete maxima, viteza maxima
+c. viteza de angajare, viteza maxima, viteza la finete
+
+45. Stratul limită este:
+a. stratul de aer din imediata vecinătate a aeronavei în care viteza creşte de la zero la viteza de zbor
+b. stratul de aer format când avionul este în limită de viteză
+c. stratul de aer care inconjoara profilul aerodinamic la unghi de incidenta critic
+
+46. Grosimea unui profil aerodinamic este:
+a. cea mai mare distanţă dintre extrados şi intrados
+b. media grosimilor secţiunilor profilului
+c. constantă.
+
+
+47. Rezultanta forţelor aerodinamice profilului are punctul de aplicatie în:
+a. centrul de presiune
+b. centrul geometric al aripii
+c. centrul de greutate
+
+48. In zbor orizontal centrul de greutate este situat:
+a. sub centrul de presiune
+b. deasupra centrului de presiune
+c. coincide cu centrul de presiune
+
+49. Cantitativ forta portanta este :
+a. variabila in functie de viteza de zbor
+b. constanta in functie de viteza de zbor
+c. constanta in toate cazurile cu exceptia angajarii
+
+50. Lungimea suspantelor poate influenta finetea parapantei:
+a. da
+b. nu
+c. numai o data cu cresterea incarcarii alare.
+
+51. Incarcarea alara se refera la :
+a. raportul dintre greutatea totala in zbor si suprafata portanta
+b. greutatea mazima in zbor
+c. greutatea maxima suspendata
+
+52. Cresterea incarcarii alare modifica performantele parapantei:
+a. da
+b. nu
+c. incarcarea alara nu are legatura cu domeniul zborului
+
+53. La actionarea in jos a comenzilor unghiul de incidenta:
+a. creste
+b. scade
+c. ramane constant
+
+54. La cresterea vitezei de scurgere a fileurilor de aer pe extrados:
+a. scade presiunea statica
+b. creste presiunea statica.
+c. presiunea statica ramane constanta
+
+55. Care dintre urmatoarele fenomene nu au legatura cu aerodinamica:
+a. legea distributiei maselor
+b. legea lui Bernoulli,
+c. efectul Coanda
+
+56. Efectul Coanda este influentat de unghiul de incidenta?
+a. da
+b. nu
+c. doar la unghiuri de incidenta subcritice
+
+57. La micsorarea unghiului de incidenta :
+a. viteza de zbor creste
+b. viteza de zbor scade
+c. viteza de zbor ramane constanta
+
+58. La cresterea unghiului de incidenta se poate ajunge la angajare numai cand:
+a. se atinge unghiul critic
+b. se atinge viteza minima de infundare
+c. se atinge viteza la finete
+
+59. Fata de zborul orizontal, in timpul virajului,  viteza de angajare :
+a. creste
+b. scade
+c. ramane neschimbata
+
+60. Forta centrifuga actioneaza toteauna:
+a. spre exteriorul virajului
+b. spre interiorul virajului
+c. spre in sus
+
+61. Cele 4 forte care actioneaza pe un profil aerodinamic sunt:
+a. greutatea, portanta, rezistenta la inaintare, viteza
+b. greutatea, portanta, forta centrifuga viteza
+c. portanta, greutatea, forta centrifuga, rezistenta la inaintare
+
+62. Fortei portante i se opune:
+a. greutatea
+b. viteza
+c. rezistenta la inaintare
+
+63. Rugozitatea unei suprafete portante duce la:
+a. cresterea rezistentei la inaintare
+b. cresterea vitezei de zbor
+c. scaderea sarcinii maxime admise
+
+64. Care dintre urmatoarele afirmatii este adevarata:
+a. finetea depinde de viteza de zbor
+b. finetea maxima se obtine la viteza de angajare (minima de zbor)
+c. finetea maxima se obtine la viteza maxima de zbor.
+
+65. Care dintre urmatoarele afirmatii este adevarata:
+a. viteza minima de infundare si finetea sunt caracteristici importante ale parapantei
+b. viteza de infundare a parapantei este indiferenta de viteza de zbor
+c. viteza minima de infundare  a parapantei se obtine la viteza minima de zbor
+
+66. Presiunea dinamica masurata in interiorul aripii variaza:
+a. in functie de viteza de zbor
+b. in functie alungirea parapantei
+c. in functie de grosimea maxima relativa a profilului parapantei
+
+67. Trompa lui Venturi demonstreaza:
+a. legea Bernoulli
+b. efectul Coanda
+c. aparitia rezistentei la inaintare
+
+68. Polara aripii de parapanta descrie:
+a. caracteristicile de zbor ale parapantei
+b. forma aripii in plan
+c. forma sheletului profilului aerodinamic
+
+
+69. La parapanta, relatia de ordonare intre Vangajare, Vmax, Vfinete-maxima si Vinfundare-minima este:
+a. Vinfundare-minima < Vangajare <  Vfinete-maxima < Vmax
+b. Vangajare <  Vinfundare-minima < Vfinete-maxima < Vmax
+c. Vangajare <  Vfinete-maxima < Vinfundare-minima < Vmax
+
+70. In momentul angajarii unei parapante
+a. Presiunea in comenzi scade brusc
+b. Viteza de inaintare creste brusc
+c. Viteza de infundare creste brusc
+
+71. Incarcarea aripii in zbor planat stabilizat este definita ca si
+a. Tensiunea din suspante exprimata in Kg/ml
+b. Greutatea pilotului plus greutatea seletei si a aparatelor de zbor
+c. Raportul dintre greutatea totala in zbor si suprafata aripii
+
+72. Ce se intampla daca marim incarcarea pe aripa:
+a. creste finetea maxima
+b. toate vitezele cresc, inclusiv viteza de angajare
+c. viteza de angajare ramine nemodificata deoarece este o caracteristica a profilului
+
+
+73. Intr-un viraj strans cu unghi de inclinare accentuat sarcina pe aripa
+a. scade
+b. creste
+c. ramine constanta doarece greutatea sistemului si suprafata aripii nu se modifica
+
+74. Rezistenta la inaintare se datoreaza:
+a. formei profilului
+b. rezistentei induse
+c. frecarii aerului cu toate componentele aflate in zbor
+
+75. La parapanta, stabilitatea in jurul axei transversale (axa ce uneste capetele aripii) se datoreaza in special
+a. vitezei relativ mici la care zboara
+b. profilului aripii
+c. efectului de pendul (centrul de greutate se afla mult mai jos decit centrul de presiune)
+
+76.  Alegeti afirmatia falsa:
+a. parapanta are profil aerodinamic
+b. parapanta nu are profil aerodinamic
+c. profilul aerodinamic al parapantei poate fi identificat ca proiectia acesteia pe un plan orizontal
+
+77. Care dintre urmatoarele notiuni se regasesc in aviatie:
+a. bord de atac, intrados
+b. tribord, velatura
+c. cordaj, rezistenta ohmica
+
+78. Alegeti afirmatia falsa:
+a. intradosul aripii este in partea inferioara a profilului
+b. extradosul aripii este in partea superioara a profilului
+c. intradosul este in partea superioara a profilului
+
+79. Alegeti afirmatia corecta:
+a. distanta dintre punctele de acrosaj a seletei modifica comportamentul parapantei in zbor
+b. distanta dintre punctele de acrosaj a seletei modifica nu comportamentul parapantei in zbor
+c. distanta dintre punctele de acrosaj a seletei este importanta la testele de omologare
+
+80. Uzura parapantei apare ca urmare a :
+a. expunerii la radiatia solara (UV)
+b. frecarii cu solul si cu microparticulele din aer
+c. presiunii dinamice din interiorul voalurii in zbor
+
+81. Greutatea totala in zbor se calculeaza:
+a. parapanta+seleta+pilot complet echipat+parasuta de siguranta
+b. pilot+seleta+parasuta de siguranta
+c. pilot+seleta
+
+82. In timpul unui viraj strans :
+a. viteza de angajare creste
+b. viteza de angajare scade
+c. viteza de angajare ramane neschimbata
+
+83. Cum se explica tendinta de viraj in cazul inchiderilor asimetrice:
+a. aripa deformata (inchisa) are rezistenta la inaintare mai mare decat cea ramasa deschisa
+b. comanda corespunzatoare aripii inchise are efect mai mare, la aceeasi cursa
+c. inchiderile asimetrice nu duc la tendinte de viraj sau autorotatie
+
+84. Care este diferenta dintre vrie si spirala picata:
+a. in cazul vriei (negativa) fileurile de aer se desprind de pe extrados
+b. in cazul spiralei picate fileurile de aer se desprind de pe extrados
+c. ambele manevre sunt identice din punct de vedere aerodinamic, este doar o diferenta de terminologie
+
+85. La ce viteza de zbor se obtine infundarea minima:
+a. mai mare decat viteza la finete maxima
+b. la viteza minima de zbor
+c. la viteza mai mica decat viteza la finete maxima
+
+86. Alegeti afirmatia corecta:
+a. finetea aerodinamica si finetea in zbor  (adevarata) pot fi diferite
+b. finetea aerodinamica si finetea in zbor (adevarata) sunt identice
+c. finetea nu se regaseste in domeniul zborului cu parapanta
+
+87. Trecerea printr-o masa de aer descendenta are ca efect:
+a. micsorarea finetei in zbor
+b. micsorarea finetei aerodinamice
+c. cresterea finetei in zbor, prin cresterea componentei verticale a vitezei
+
+
+88. Alegeti afirmatia corecta:
+a. comenzile actioneaza asupra bordului de atac
+b. cu ajutorul comenzilor modificam unghiul de incidenta
+c. manevra URECHI se face cu ajutorul comenzilor
+
+
+
+89. Stabilitatea pe directie a parapantei este asigurata de:
+a. bordul de fuga
+b. efectul de pendul
+c. stabilizatoare (capetele de plan)
+
+90. Membranele intercheson au rolul :
+a. de a mentine presiunea dinamica in interiorul voalurii
+b. de a prelua sarcina de pe extrados
+c. de a materializa profilul aerodinamic al parapantei
+
+91. Polara aripii reprezinta:
+a. curbura extradosului
+b. curbura bordului de fuga reprezentata in plan
+c. o sinteza a caracteristicilor de zbor
+
+92. Alegeti care dintre cele 3 obiecte are rezistenta la inaintare mai mica:
+a. picatura de apa
+b. sfera
+c. paralelipiped
+
+93. Alegeti afirmatia corecta:
+a. din polara putem deduce regimurile de zbor ale parapantei
+b. din polara putem deduce sarcina utila a parapantei
+c. din polara putem deduce raza maxima de viraj a parapantei
+
+94. Alegeti afirmatia corecta:
+a. portanta este generata 50% pe extrados
+b. portanta este generata 2/3 pe extrados
+c. portanta este generata exclusiv pe intrados
+
+95. Portanta este generata de :
+a. presiunea din voalura
+b. presiunea dinamica din zona bordului de fuga
+c. diferenta de presiune dintre extrados si intrados
+
+96. Care dintre urmatoarele forte se regasesc in principiul celor 4 forte:
+a. portanta, rezistenta la inaintare, viteza, greutate
+b. portanta, rezistenta ohmica, viteza verticala, viteza orizontala
+c. portanta, viteza verticala, viteza orizontala, greutate
+
+97. Alegeti afirmatia corecta:
+a. portanta este liniara
+b. rezistenta la inaintare este direct proportionala cu patratul vitezei
+c. rezistenta la inaintare scade brusc la unghi critic
+
+98. Alegeti afirmatia corecta:
+a. traseul fileurilor de aer pe extrados este egal cu cel pe intrados
+b. traseul fileurilor de aer pe extrados este mai lung
+c. traseul fileurilor de aer pe intrados este mai lung
+
+99. Grosimea stratului limita este influentata de:
+a. rugozitatea (calitatea) suprafetei aripii
+b. porozitatea materialului
+c. sarcina alara
+
+100. Unghiul de incidenta se masoara:
+a. fata de orizontala
+b. fata de directia de zbor
+c. intre corda profilului si directia de curgere a fileurilor de aer
+
+101. Functionarea parasutei de rezerva de tip circular (calota) se bazeaza pe:
+a. efect coanda
+b. rezistenta la inaintare
+c. portanta la viteza zero";
+
+
+$str = iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $str);
+
+
+$arr = explode(PHP_EOL,$str);
+$arrInsert = array();
+foreach($arr as $row) {
+    if(empty($row)) continue;
+    $arrRow = explode(".",$row);
+    if(is_numeric($arrRow[0])) {
+        if(!empty($arrQuestions))
+            $arrInsert[] = $arrQuestions;
+        //limita intre intrebari
+        $arrQuestions = array("category"=>"Principiile zborului");
+
+        $j=0;
+        $arrQuestions["category_index"] = trim($arrRow[0]);
+        $arrQuestions["text"] = $arrRow[1];
+    } else {
+        $j++;
+        $arrQuestions["answer_$j"] = $arrRow[1];
+    }
+
+}
+if(!empty($arrQuestions))
+    $arrInsert[] = $arrQuestions;
+
+
+$handler = mysqli_connect("localhost");
+mysqli_select_db($handler,"test_repo");
+
+function replace_special_chars($string) {
+    $string = strtr($string,"ăâîșțĂÂÎȘȚ","aaistAAIST");
+    return $string;
+}
+
+$id = 100;
+foreach($arrInsert as $data) {
+    $id++;
+    $query = "INSERT INTO questions SET id=$id,";
+    $values = array();
+
+    foreach($data as $field=>$value) {
+        $values[] = "$field = '$value'";
+    }
+
+    $query .= implode(",",$values);
+    //$query = replace_special_chars($query);
+    mysqli_query($handler,$query);
+    //echo $query."<br />";
+}
