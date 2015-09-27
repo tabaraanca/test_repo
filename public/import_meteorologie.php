eb6aa4d8766a6a3e63eb9703f54c2a25a513ecab
Dan Dumitriu <dan.dumitriu@sfappworks.com>
2015-09-27 19:32:32 +0300

    import script meteo

---
+++ public/import_meteorologie.php
@@ -0,0 +1,360 @@
+<?php
+$str = '
+1. Meteorologia este:
+a) stiinta care se ocupa cu studiul atmosferei si a fenomenelor din atmosfera;
+b) disciplina care se ocupa cu studiul prognozei meteorologice;
+c) stiinta care se ocupa cu predictia vremii.
+
+2. A.N.M este:
+a) Inspectoratul National de Meteorologie si Hidrologie;
+b) Administratia Nationala de Meteorologie;
+c) Interpretarea Nationala de Meteorologie si Hidrologie.
+
+3. Ce este atmosfera?
+a) invelisul gazos al globului pamantesc, cunoscut sub denumirea de aer;
+b) invelisul gazos al pamantului pana la o inaltime de 30.000m;
+c) invelisul gazos al pamantului pana la stratosfera.
+
+4. Aerul este compus din:
+a) vapori de apa, particule microscopice, fum;
+b) praf, micrometeoriti, saruri, bacterii;
+c) amestec de gaze.
+
+5. Dintre gazele componente ale atmosferei, procentajul cel mai mare in componenta aerului uscat il are:
+a) oxigenul;
+b) bioxidul de carbon;
+c) azotul.
+
+6. Zonele atmosferei sunt:
+a) troposfera, stratosfera, mezosfera, termosfera,
+b) troposfera, stratosfera, mezosfera;
+c) stratosfera, mezosfera, termosfera.
+
+7. In care zona a atmosferei au loc toate fenomenele obisnuite din natura: ploaie, fulgere, trasnete?
+a) mezosfera;
+b) troposfera;
+c) tropopauza.
+
+8. In ce zona a atmosferei zburam cu parapanta?
+a) ionosfera;
+b) strat de ozon;
+c) troposfera.
+9. Factorii care caracterizeaza aerul atmosferic sunt:
+a) vantul, presiunea si temperatura;
+b) umiditatea, presiunea si vantul;
+c) presiunea, temperatura si umiditatea.
+
+10. Aerul poate fi caracterizat ca si:
+a) omogen;
+b) regulat;
+c) neregulat.
+
+11. Presiunea atmosferica este:
+a) apasarea exercitata de o coloana de aer avand suprafata bazei de 1cm&sup2; si inaltimea egala cu inaltimea atmosferei;
+b) apasarea exercitata de o coloana de aer avand suprafata bazei de 1cm&sup2; si inaltimea egala cu inaltimea troposferei;
+c) apasarea exercitata de o coloana de aer avand suprafata bazei de 1cm&sup2; si inaltimea egala cu inaltimea tropopauzei.
+
+12. Unitatea de masura a presiunii atmosferice este:
+a) hectopascal;
+b) milibari;
+c) milimetrul coloana de mercur.
+
+13. Masurarea presiunii atmosferice se poate realiza cu ajutorul:
+a) hectometrul;
+b) variometrul;
+c) barometrul.
+
+14. Presiunea atmosferica scade in altitudine datorita:
+a) scaderii densitatii aerului in inaltime;
+b) scurtarii coloanei de aer odata cu cresterea inaltimii;
+c) scaderii umiditatii.
+
+15. Prin definitie, treapta barica reprezinta:
+a) distanta pe verticala, in metri, pentru care se inregistreaza o crestere a presiunii atmosferice cu 1 milibar;
+b) distanta pe verticala, in metri, pentru care se inregistreaza o descrestere a presiunii atmosferice cu 1 milibar;
+c) distanta pe orizontala, in metri, pentru care se inregistreaza o descrestere a presiunii atmosferice cu 1 milibar.
+
+16. Vantul este miscarea aerului pe:
+a) orizontala;
+b) verticala;
+c) diagonala.
+
+17. Miscarile verticale si/sau inclinate ale aerului se numesc:
+a) curenti;
+b) vanturi;
+c) rafale.
+18. De ce este provocat vantul:
+a) de diferenta de presiune pe verticala;
+b) de diferenta de presiune pe diagonala;
+c) de diferenta de presiune pe orizontala.
+
+19.  Incalzirea suprafetei terestre deci si a maselor de aer din vecinatatea acestora este:
+a) egala;
+b) omogena;
+c) inegala.
+
+20. Marimile ce definesc vantul sunt:
+a) directia;
+b) intensitatea (si viteza)
+c) densitatea.
+
+21. Prin directia vantului, in meteorologie se intelege:
+a) directia inspre care "sufla" (vine) vantul;
+b) directia de unde "sufla" (vine) vantul;
+c) nordul magnetic.
+
+22. 1m/s este egal cu:
+a) 2,8 km/h;
+b) 3,6 km/h;
+c) 12km/h.
+
+23. Liniile care unesc punctele cu aceeasi presiune atmosferica:
+a) izometre;
+b) izoterme;
+c) izobare.
+
+24. Distanta pe verticala masurata de la nivelul mediu al marii pana la punctul considerat unde se afla aeronava se numeste:
+a) altitudine;
+b) inaltimea reala;
+c) inaltime
+
+25. Care din urmatoarele afirmatii sunt corecte:
+a) QNH = presiunea redusa la nivelul marii;
+b) QFE = presiunea la nivelul pistei;
+c) QNE (Std) = presiunea de referinta de 760 mm col Hg sau 1013.25Mb (HPa).
+
+26. Pamantul se incalzeste de la soare printr-un fenomen numit:
+a) conductie;
+b) convectie;
+c) radiatie.
+
+27. Izotermele reprezinta:
+a) liniile ce unesc toate punctele cu aceeasi temperatura;
+b) liniile ce unesc toate punctele cu aceeasi presiune;
+c) liniile ce unesc toate punctele cu aceeasi altitudine.
+28. Masa calda de aer este obligata sa urce pe panta aerului rece, si prin detenta are loc racirea adiabatica, conditie care determina ajungerea masei de aer la saturatie, fenomen denumit:
+a) convectie frontala;
+b) convectie orografica;
+c) convectie orizontala.
+
+29. Masa calda de aer este obligata sa urce pe panta montana, si prin detenta are loc racirea adiabatica, conditie care determina ajungerea masei de aer la saturatie, fenomen denumit:
+a) convectie frontala;
+b) convectie orografica;
+c) convectie orizontala.
+
+30. Variatia temperaturii pentru o diferenta de nivel de 100 m se numeste:
+a) gradientul termic vertical;
+b) gradientul orizontal;
+c) gradientul adiabatic.
+
+31. Valoarea gradientului termic vertical mediu este:
+a) 1&deg;C/100 m;
+b) 0,5&deg;C/100 m;
+c) 0,65&deg;C/100m.
+
+32. Spunem despre atmosfera ca este instabila daca:
+a) miscarile verticale produse la un moment dat, nu pot sa se dezvolte si deci inceteaza;
+b) miscarile verticale care se produc se propaga de la un nivel la altul;
+c) curentii ascendenti formati in urma unui impuls oarecare se vor opri din miscarea acensionala.
+
+33. Spunem despre atmosfera ca este stabila daca:
+a) curentii ascendenti formati in urma unui impuls oarecare se vor opri din miscarea acensionala;
+b) curentii ascendenti tind sa-si continue miscarea impulsionati de o accelaratie noua;
+c) miscarile orizontale produse la un moment dat, nu pot sa se dezvolte si deci inceteaza.
+
+34. Zona din troposfera in care temperatura creste odata cu cresterea de inaltime se numeste zona de:
+a) Inversiune termica;
+b) izotermie;
+c) condensare.
+
+35. Izotermia se defineste ca zona:
+a) in care temperatura este stationara cu cresterea de inaltime;
+b) in care temperatura creste odata cu cresterea de inaltime;
+c) in care temperatura scade odata cu cresterea de inaltime.
+
+36. In cazul in care avem inversiune termica la sol se poate produce:
+a) polei;
+b) chiciura;
+c) ceata.
+37. Care din urmatoarele afirmatii este falsa?
+a) o masa de aer cald urca peste o masa de aer rece;
+b) o masa de aer rece in miscare disloca o masa de aer cald pe care o obliga sa urce;
+c) o masa de aer cald coboara sub o masa de aer rece;
+
+
+38. Temperatura standard in aviatie este:
+a) 15&deg;C la 760 mm Hg;
+b) 10&deg;C la 760 mm Hg;
+c) 0&deg;C la 760 mm Hg.
+
+39. Temperatura punctului de roua este:
+a) temperatura la care aerul devine saturat prin racire la o presiune constanta;
+b) temperatura la care aerul devine saturat prin racire la o presiune in crestere;
+c) temperatura la care aerul devine saturat prin incalzire la o presiune constanta.
+
+40. Ce se intampla cu o masa de aer in urcare datorita scaderii presiunii?
+a) aerul se contracta;
+b) aerul se dilata;
+c) aerul nu-si modifica volumul.
+
+41. Ce reprezinta nivelul de condensare?
+a) Inaltimea la care apar norii;
+b) altitudinea la care umezeala relativa devine 100%;
+c) inaltimea la care umezeala relativa devine 100%.
+
+42. In mod obisnuit, ploaia cade din:
+a) Nimbostratus;
+b) Stratus;
+c) Stratocumulus.
+
+43. Centru de minima presiune caracterizata prin descresterea presiunii catre centrul sistemului se numeste:
+a) anticiclon;
+b) ciclon;
+c) anticiclon, ciclon.
+
+44. Vantul are o miscare de la exterior spre centru sau in sens invers acelor de ceasornic (pentru emisfera nordica).
+a) in cazul ciclonului;
+b) in cazul izotermei;
+c) in cazul anticiclonului.
+
+45. Centru de maxima presiune caracterizata prin cresterea presiunii de la exterior spre centrul sistemului se numeste:
+a) anticiclon;
+b) ciclon;
+c) anticiclon, ciclon.
+
+46. Vantul are o miscare de la centru spre exterior sau in sensul acelor de ceasornic (pentru emisfera nordica).
+a) in cazul ciclonului;
+b) in cazul izotermei;
+c) in cazul anticiclonului.
+
+47. Forta care produce miscarea orizontala, in cazul aerului este:
+a) umiditatea;
+b) diferenta de presiune;
+c) temperatura.
+a) insolatie.
+
+48. Norii convectivi formati prin convectie termica sau dinamica sunt:
+a) Cu, Cb;
+b) Ci, As, St;
+c) As, Ns.
+
+49. Norii se clasifica dupa inaltimea bazei fata de sol in cate categorii:
+a) 3;
+b) 4;
+c) 5.
+
+50. Norii cumulus sunt nori de:
+a) inalta inaltime;
+b) joasa inaltime;
+c) medie inaltime.
+
+51. In care nori intalnim descarcari electrice:
+a) Cumulus humilis;
+b) Cumulonimbus;
+c) Altocumulus.
+
+52. Frontul in lungul caruia aerul rece in deplasare inlocuieste aerul cald este:
+a) frontul rece;
+b) frontul cald;
+c) frontul oclus
+
+53. Frontul cald se dezvolta atunci cand:
+a) masa de aer cald impinge si inlocuieste o masa de aer rece;
+b) masa de aer rece impinge si inlocuieste o masa de aer cald;
+c) masa de aer rece impinge si inlocuieste o masa de aer mai rece.
+
+54. Atunci cand aerul rece si cel cald din doua mase de aer invecinate se deplaseaza paralel, in acelasi sens sau chiar in sens invers, dar niciuna dintre ele nu o poate inlocui pe cealalta, avem parte de un:
+a) front oclus;
+b) front stationar;
+c) front rece.
+
+
+55. Ce este gradientul termic vertical ?
+a) gradientul termic vertical reprezinta variatia temperaturii pentru o diferenta de nivel de  100 m.
+b) miscarea orizontala a aerului.
+c) diferenta de viteza a vantului pe diferite altitudini din 100 in 100 de metri.
+
+       72.  Cand se poate afirma ca este vant laminar ?
+a) cand nu sunt rafale.
+b) iarna.
+c) in situatia cand vantul are o miscare uniforma, atat in ce priveste viteza cat si directia,scurgerea aerului facandu-se in straturi paralele
+
+     74. Din ce sunt constituiti norii ?
+a) din aer cald.
+b) din picaturi fine de apa sau cristale de gheata in suspensie in atmosfera.
+c) din bule termice.
+
+          77. Este indicat sa zburam in apropierea unui Cumulonimbus (CB) ?
+     	a) da
+b) nu
+c) depinde de directia vantului
+
+
+     78.Cirrus, cirrocumulus si cirrostratus sunt nori periculosi pentru parapantism ?
+a) da
+b) nu
+c) uneori
+';
+
+
+$str = iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $str);
+
+
+$arr = explode(PHP_EOL,$str);
+$arrInsert = array();
+foreach($arr as $row) {
+    if(empty($row)) continue;
+    $arrRow = explode(".",$row);
+    if(is_numeric($arrRow[0])) {
+        if(!empty($arrQuestions))
+            $arrInsert[] = $arrQuestions;
+        //limita intre intrebari
+        $arrQuestions = array("category"=>"Meteorologie");
+
+        $j=0;
+        $arrQuestions["category_index"] = trim($arrRow[0]);
+        unset($arrRow[0]);
+        $arrQuestions["text"] = implode(".",$arrRow);
+
+    } else {
+        $arrRow2 = explode(")",$row);
+        if(in_array(trim($arrRow2[0]),array("a","b","c"))) {
+            $j++;
+            unset($arrRow2[0]);
+            $arrQuestions["answer_$j"] = implode(")",$arrRow2);
+        } else {
+            $j++;
+            unset($arrRow[0]);
+            $arrQuestions["answer_$j"] = implode(".",$arrRow);
+        }
+    }
+
+
+}
+if(!empty($arrQuestions))
+    $arrInsert[] = $arrQuestions;
+
+
+//echo "<pre>"; var_dump($arrInsert); exit;
+
+$handler = mysqli_connect("localhost");
+mysqli_select_db($handler,"test_repo");
+
+
+$id = 400;
+foreach($arrInsert as $data) {
+    $id++;
+    $query = "INSERT INTO questions SET id=$id,";
+    $values = array();
+
+    foreach($data as $field=>$value) {
+        $values[] = "$field = '$value'";
+    }
+
+    $query .= implode(",",$values);
+    mysqli_query($handler,$query);
+    echo $query."<br />";
+}
+
+?>
+
