e81e541f9d8883e893363ae1a4f1a9a695bac75e
Dan Dumitriu <dan.dumitriu@sfappworks.com>
2015-09-27 19:54:44 +0300

    import script cunoasterea aeronavei

---
+++ public/import_cunoasterea_aeronavei.php
@@ -0,0 +1,408 @@
+<?php
+$str = "
+
+1. Actionarea acceleratorului are ca efect:
+a. reducerea vitezei de zbor
+b. dublarea vitezei de zbor
+c. cresterea vitezei de zbor
+
+2. O parapanta omologata LTF – 1 respectiv EN – A este destinata:
+a. pilotilor de competitie
+b. pilotilor incepatori
+c. exclusiv pilotilor de acrobatie
+
+3. Riscul de twist este mai mare cand:
+a. chinga de piept este foarte stransa
+b. folosim seleta este de tip cocoon
+c. chinga de piept este foarte larga
+
+4. Tensiunea in comenzi creste datorita:
+a. cresterii presiunii in voalura
+b. cresterii unghiului de incidenta
+c. cresterii vitezei de zbor
+
+5. O parapanta omologata LTF 2 (EN –C) este destinata:
+a. exclusiv pilotilor incepatori
+b. exclusiv zborului in tandem
+c. exclusiv pilotilor cu nivel mediu/ridicat de pregatire
+
+6. Alungirea aripii este:
+a. raportul dintre coarda medie si anvergura
+ b.raportul dintre profunzimea maxima a aripii si anvergura
+c. raportul dintre grosimea maxima relativa a profilului si anvergura
+
+7. Aripa de parapanta prezinta torsiune geometrica:
+a. da
+ b. nu
+c. torsiunea geometrica nu are legatura cu aerodinamica
+
+8. Profilul aripii este dat de :
+a. membranele intercheson
+b. proiectia aripii pe un plan orizontal
+c. suprafata aripii desfasurata
+
+9. Lungimea suspantelor poate influenta finetea parapantei:
+a. da
+b. nu
+c. numai o data cu cresterea incarcarii alare.
+
+
+10. Incarcarea alara se refera la :
+a. raportul dintre greutatea totala in zbor si suprafata portanta
+b. greutatea maxima in zbor
+c. sarcina nimima si maxima a parapantei
+
+11. La actionarea in jos a comenzilor unghiul de incidenta:
+a. creste
+b. scade
+c. ramane constant
+
+12. Alegeti afirmatia falsa:
+a. parapanta are profil aerodinamic
+b. parapanta nu are profil aerodinamic
+c. profilul aerodinamic al parapantei poate fi identificat ca proiectia acesteia pe un plan orizontal
+
+13. Nodul pe o suspanta are drept efect:
+a. scade rezistenta la rupere a suspantei respective
+b. creste rezistenta la rupere a suspantei respective
+c. nu influenteaza rezistenta la rupere a suspantelor
+
+14. Alegeti afirmatia adevarata:
+a. comenzile actioneaza asupra stabilizatoarelor
+b. comenzile actioneaza asupra liniei de suspante B
+c. comenzile actioneaza asupra bordului de fuga
+
+15. Suspantele fac legatura dintre:
+a. cordul de atac, bordul de fuga si seleta
+b. voalura si chingi portsuspante
+c. voalura si carabinierele de acrosaj
+
+16. Bordul de fuga este:
+a. partea din fata a profilului
+b. partea centrala a  aripii
+c. partea din spate a aripii
+
+17. Alegeti afirmatia falsa:
+a. intradosul aripii este in partea inferioara a profilului
+b. extradosul aripii este in partea superioara a profilului
+c. intradosul este in partea superioara a profilului
+
+18. Alegeti afirmatia corecta:
+a. nodurile slabesc rezistenta la rupere a suspantelor
+b. radiatia ultravioleta degradeaza kevlarul
+c. suspantele din kevlar nu au nevoie de manta de protectie
+
+19. Tesatura din care este construita voalura trebuie sa fie:
+a. deformabila atat pe urzeala cat si pe batatura
+b. nedeformabila pe urzeala si pe batatura
+c. elastica pe orice directie
+
+20. Alegeti afirmatia corecta:
+a. distanta dintre punctele de acrosaj a seletei modifica comportamentul parapantei in zbor
+b. distanta dintre punctele de acrosaj a seletei modifica nu comportamentul parapantei in zbor
+c. distanta dintre punctele de acrosaj a seletei este importanta la testele de omologare
+
+21. Omolgarea parapantei arata:
+a. performantele parapantei: finete maxima, viteza minima de infundare, viteza maxima
+b. nivelul de pilotaj necesar
+c. factorul de sarcina admis
+
+22. Alegeti afirmatia corecta:
+a. comenzile actioneaza asupra bordului de atac
+b. cu ajutorul comenzilor modificam unghiul de incidenta
+c. manevra URECHI se face cu ajutorul comenzilor
+
+23. Stabilitatea pe directie a parapantei este asigurata de:
+a. bordul de fuga
+b. efectul de pendul
+c. stabilizatoare (capetele de plan)
+
+24. Membranele intercheson au rolul :
+a. de a mentine presiunea dinamica in interiorul voalurii
+b. de a prelua sarcina de pe intrados
+c. de a materializa profilul aerodinamic al parapantei
+
+25. Caracteristicile si performantele de zbor ale parapantei sunt influentate de:
+a. gradul de uzura al voalurii
+b. lungimea suspantelor
+c. altitudinea de zbor
+
+26. Acceleratorul modifica:
+a. unghiul de incidenta
+b. scheletul profilului
+c. coarda profilului
+
+
+
+
+27. Omologarea parapantelor are drept scop:
+a. verificarea performantelor de zbor
+b. verificarea gradului de securitate
+c. ambele de la pct a. si b.
+
+28. Alegeti afirmatia corecta:
+a. seletele tip cocoon au rezistenta la inaintare mai mica fata de seletele clasice
+b. seletele tip cocoon au rezistenta la inaintare mai mare fata de seletele clasice
+c. seletele tip cocoon au protectie la impact mai buna decat seletele clasice
+
+29. Alegeti afirmatia corecta:
+a. sistanta dintre punctele de acrosaj are influenta asupra comportamentului parapantei
+b. sistanta dintre punctele de acrosaj are influenta doar asupra confortului pilotului
+c. sistanta dintre punctele de acrosaj se regleaza intr-un domeniu strict
+
+30. Voalura este confectionata din material sintetic in carouri( Rip-Stop) pentru:
+a. a mentine mai bine presiunea in voalura
+b. pentru a mari rezistenta materialului
+c. a limita extinderea in caz de ruptura
+
+31.   Seletele dotatate cu protector dorsal sunt recomandata:
+a. doar elevilor piloti in perioada de scolarizare
+b. tuturor pilotilor indiferent de nivel
+c. doar elevilor cu nivel superior de pregatire
+
+32. Suspantele parapantei sunt:
+a. elasice
+b. statice
+c. pe linia A si B sunt statice iar C si D elastice
+
+33. Cate categorii de greutate regasim la parapante:
+a. fiecare producator are intervale de greutate proprii, conform omologarilor
+b. sunt 3 nivele : S, M si L
+c. sunt 4 nivele: S,M,L si XL
+
+34. Incarcarea peste limita superioara a parapantei are ca efect:
+a. cresterea tuturor vitezelor
+b. scaderea vietezei la finete maxima
+c. scaderea vitezei minime de infundare
+
+35. Care este numarul standard de chingi portsuspante la parapante:
+a. nu exista un numar standard
+b. 4
+c. 5
+
+36. Care sunt riscurile ce apar o data cu uzura voalurii:
+a. finetea maxima se obtine doar cu acceleratorul actionat la maximum
+b. pericol de angajare prematura si deepstall
+c. scade viteza minima de infundare
+37. Alegeti afirmatia corecta:
+a. modificarea configuratiei suspantelor altereaza gradul de securitate in zbor
+b. modificarea configuratiei suspantelor altereaza doar performantele de zbor ale parapantei
+c. modificarea configuratiei suspantelor nu este critica decat asupra maneabilitatii parapantei
+
+38. Alegeti afirmatia corecta:
+a. prelungirea suspantelor de comanda duce la marirea vitezei maxime de zbor
+b. prelungirea suspantelor de comanda duce la marirea finetii in zbor
+c. prelungirea suspantelor de comanda duce la marirea cursei comenzilor spre in jos
+
+39. Alegeti categoria cu care este posibil zborul de distanta (XC):
+a. LTF 1 / EN-A
+b. LTF 1-2 /  EN-B
+c. LTF 2/ EN-C
+
+40. Pentru a zbura cu pasager este necesar:
+a. parapanta speciala pentru zborul cu pasager
+b. parapanta omologata LTF 1 / EN A
+c. parapanta omologata LTF 2-3 / EN-D
+
+41. Finetea parapantei este direct influentata de :
+a. lungimea suspantelor
+b. alungirea aripii
+c. clasa de omologare
+
+42. Seleta tip cocoon  influenteaza:
+a. viteza minima de infundare
+b. finetea parapantei
+c. viteza minima de infundare
+
+43. Ce regim de zbor este potrivit la o aripa LTF1 / ENA pentru spiralarea in termica:
+a. regim de zbor la finete
+b. regim de zbor la infundare minima
+c. regim de zbor la viteza maxima
+
+44. Cate regimuri de zbor prezinta parapanta:
+a. 3
+b. 2
+c. 5
+
+45. Care este zona de uzura maxima a voalurii de parapanta?
+a. extrados treimea anterioara, masurand de la bordul de atac
+b. tot extradosul
+c. tot intradosul
+
+46. Depasirea intervalului de greutate poate sa duca la:
+a. modificarea comportamentului parapantei
+b. modificarea performantelor parapantei
+c. modificarea formei parapantei
+47. Unghiul de incidenta se masoara:
+a. fata de orizontala
+b. fata de directia de zbor
+c. intre corda profilului si directia de curgere a fileurilor de aer
+
+48. Alegeti afirmatia corecta:
+a. toate suspantele sunt fixe
+b. suspantele de comanda sunt mobile
+c. toate suspantele sunt mobile
+
+49. Alegeti afirmatia corecta:
+a. orice parapanta poate fi lansata prin remorcaj la mosor
+b. orice parapanta poate fi folosita la zbor cu motor
+c. orice parapanta poate fi folosita la exercitii de gonflaj la sol
+
+50. Constructiv voalura se compune din:
+a. extrados, intrados, membrane intercheson
+b. extrados, intrados, bord de atac, bord de fuga
+c. extrados, intrados, stabilizatoare
+
+51. Alegeti elementele care duc la uzura prematura a volaurii:
+a. expunere la radiatie ultravioleta
+b. zbor in atmosfera salina
+c. remorcaj la mosor
+
+52. Scheletul profilului este materializat de:
+a. forma profilului aerodinamic
+b. totalitatea elemntelor de ranforsare din interiorul aripii
+c. nu este materializat
+
+53. O parapanta cu mai multe membrane intercheson va avea:
+a. finete mai mare
+b. finete mai mica
+c. viteza de angajare mai mica
+
+54.  La parapanta centrul de greutate este:
+a. la nivelul voalurii
+b. la nivelul pilotului
+deasupra voalurii
+
+55. Alegeti afirmatia corecta:
+a. profilul aerodinamic al parapantei se regaseste in proiectia voalurii pe un plan orizontal
+b. parapanta nu are profil aerodinamic ci doar extrados, intrados, bord de atac si bord de fuga
+c. profilul aerodinamic al parapantei este materializat de membranele intercheson
+
+56. Alegeti afirmatia corecta
+a. polara se gaseste pe extrados
+b. polara se gaseste pe intrados
+c. polara este o forma sintetica de descriere a performantelor de zbor
+57. In cazul aripii de parapanta regasim:
+a. torsiune geometrica
+b. numai torsiune transversala
+c. numai torsiune aerodinamica
+
+58. In zbor orizontal centrul de greutate este situat:
+a. sub centrul de presiune
+b. deasupra centrului de presiune
+c. coincide cu centrul de presiune
+
+59. Alegeti afirmatia corecta:
+a. toate parapantele au profil reflex
+b. toate parapantele au profil aerodinamic
+c. parapantele nu au profil aerodinamic
+
+60. Alegeti afirmatia corecta:
+a. suspantele parapantei sunt elastice
+b. toate suspantele parapantei sunt fixe
+c. suspantele de comanda nu sunt fixe
+
+61. Alegeti afirmatia corecta:
+a. din polara putem deduce sarcina utila, viteza maxima si viteza minima a parapantei
+b. din polara putem deduce regimurile de zbor ale parapantei
+c. din polara putem deduce forma profilului parapantei
+
+62. Alegeti afirmatia corecta:
+a. aripile omologate LTF 2-3 / EN –D se folosesc la scolarizare
+b. aripile omologate LTF 2-3 / EN –D se folosesc de regula de catre piloti cu minimum de experienta
+c. aripile omologate LTF 2-3 / EN –D se folosec doar de piloti avansati
+
+63.  Alegeti categoria de omologare folosita pentru aripile de scoala:
+a. LTF 1 / EN-A
+b. LTF 2 / EN-C
+c. LTF 2-3 / EN-D
+
+64. Alegeti clasa de omologare cu nivelul de securitate cel mai ridicat:
+a. LTF 1 / EN-A
+b. LTF 1-2 / EN-B
+c. LTF 2  / EN-C
+
+65. O aripa cu vechime de 15 ani, care a zburat in acest interval de timp, prezinta:
+a. uzura fizica
+b. uzura morala
+c. uzura fizica si morala
+
+
+66. Intervalul de greutate se refera la :
+a. greutatea totala in zbor: pilot echipat+seleta+parasuta de rezerva+parapanta
+b. greutatea suspendata: pilot echipat+seleta+parasuta de rezerva
+c. greutatea pilotului complet echipat
+
+67. Alegeti afirmatia corecta:
+a. parapantele se comporta identic la nivelul marii sau la altitudini peste 6000m
+b. performantele si comportamentul parapantei depind de densitatea aerului
+c. performantele si comportamentul parapantei depind numai de greutatea totala in zbor
+
+68. Stabilizatoarele parapantei au rol de:
+a. stabilizeaza parapanta pe directie
+b. stabilizeaza unghiul de incidenta al parapantei
+c. stabilizeaza unghiul de panta al parapantei
+";
+
+
+$str = iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $str);
+
+
+$arr = explode(PHP_EOL,$str);
+$arrInsert = array();
+foreach($arr as $row) {
+    if(empty($row)) continue;
+    $arrRow = explode(".",$row);
+    if(is_numeric($arrRow[0])) {
+        if(!empty($arrQuestions))
+            $arrInsert[] = $arrQuestions;
+        //limita intre intrebari
+        $arrQuestions = array("category"=>"Cunoasterea aeronavei");
+
+        $j=0;
+        $arrQuestions["category_index"] = trim($arrRow[0]);
+        unset($arrRow[0]);
+        $arrQuestions["text"] = implode(".",$arrRow);
+
+    } else {
+        $arrRow2 = explode(")",$row);
+        if(in_array(trim($arrRow2[0]),array("a","b","c"))) {
+            $j++;
+            unset($arrRow2[0]);
+            $arrQuestions["answer_$j"] = implode(")",$arrRow2);
+        } else {
+            $j++;
+            unset($arrRow[0]);
+            $arrQuestions["answer_$j"] = implode(".",$arrRow);
+        }
+    }
+
+
+}
+if(!empty($arrQuestions))
+    $arrInsert[] = $arrQuestions;
+
+
+//echo "<pre>"; var_dump($arrInsert); exit;
+
+$handler = mysqli_connect("localhost");
+mysqli_select_db($handler,"test_repo");
+
+
+$id = 600;
+foreach($arrInsert as $data) {
+    $id++;
+    $query = "INSERT INTO questions SET id=$id,";
+    $values = array();
+
+    foreach($data as $field=>$value) {
+        $values[] = "$field = '$value'";
+    }
+
+    $query .= implode(",",$values);
+    mysqli_query($handler,$query);
+    echo $query."<br />";
+}
+
+?>
+
