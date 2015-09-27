427f2d51013583095a3d72601d5c811f879bcd7d
Dan Dumitriu <dan.dumitriu@sfappworks.com>
2015-09-27 19:12:40 +0300

    import script navigatie

---
+++ public/import_navigatie.php
@@ -0,0 +1,320 @@
+<?php
+$str = "
+1. Ce este navigatia aeriana
+a) o informare
+b) un sistem de deplasare al aeronavelor
+c) o stiinta
+
+2. Care este metoda de navigatie pentru zborul in VFR
+a) observata
+b) estimata
+c) izobarica
+
+3. Cum sunt numerotate meridianele
+a) de la est la vest
+b) de la vest la est
+c) in ambele sensuri
+
+4. Cum sunt numerotate paralelele
+a) de la nord la sud
+b) de la ecuator spre sud
+c) de la ecuator spre nord si sud
+
+5. Ce reprezinta nivelmentul
+a) totalitatea formelor de relief
+b) forme de relief specifice hartilor
+c) reprezinta planeitatea terenului
+
+6. Care sunt directiile principale
+a) N , NV ,  E, SE
+b) N , S , E , V
+c) E, SE, V, NV
+
+7. Care sunt Nordurile folosite in navigatie
+a) Nordul adevarat
+b) Nordul magnetic
+c) Nordul izobaric
+
+8. Ce este declinatia magnetica
+a) directia compas
+b) unghiul dintre aeronava si Nord
+c) unghiul dintre Nordul magnetic si Nordul adevarat
+
+
+
+9. Ce este distanta
+a) spatiul dintre doua puncte
+b) lungimea liniei care le uneste
+c) unitatea de masura
+
+10. Ce unitate de masura este folosit pentru inaltime in normele OACI
+a) yardul
+b) foot
+c) inch-ul
+
+11. Care este scara hartii pentru zborul in VFR
+a) 1: 200 000
+b) 1: 50 000
+c) 1: 500 000
+
+12. Cum se numeste meridianul prim
+a) Pascal
+b) Greenwich
+c) Hecto
+
+13. Cum se numeste traseul de la decolare pana la aterizare
+a) itinerar
+b) traiect
+c) ruta
+
+14. Pentru un zbor in VFR este nevoie de o harta
+a) informativa
+b) operativa
+c) de ruta
+
+15. Care sunt reperele liniare pe o harta
+a) orase, lacuri, terenuri
+b) caii ferate , sosele, rauri
+c) poduri mari, fabrici izolate, silozuri
+
+16.  La o rotatie de 24 de ore a pamantului corespund
+a) 180&deg; longitudine
+b) 230&deg; longitudine
+c) 360&deg; longitudine
+
+17.  In ce se masoara viteza vantului
+a) m/s
+b) noduri
+c) km/h
+18.  Ce este UTC
+a) timpul local
+b) ora actuala
+c) timpul universal coordonat
+
+19.  Care sunt etapele planificarii zborului
+a) alegerea hartilor
+b) estimarea meteo
+c) trasarea traiectului
+
+20.   Ce este GPS
+a) sistem de navigatie
+b) sistem de control
+c) sistem de pozitie
+
+21.  Pozitionarea in sistem GPS se face prin
+a) sistem radio
+b) triangulatie
+c) balize
+
+22.   Cate surse minim foloseste sistemul GPS pentru stabilirea pozitiei si altitudinii
+a) 3
+b) 5
+c) 4
+
+23.  Cate surse minim foloseste sistemul GPS pentru stabilirea pozitiei
+a) 3
+b) 12
+c) 9
+
+24.   Ce este o altitudine barometrica
+a) o inaltime de la nivelul marii
+b) o inaltime care corespunde presiunii standard
+c) o inaltime dintr-un punct de referinta
+
+25. Din ce directie bate vantul la 135&deg;
+a) NE
+b) E
+c) SE
+
+26. Din ce directie bate vantul la 270&deg;
+a) N
+b) W
+c) SW
+
+
+27. Din ce directie bate vantul la 0&deg;
+a) N
+b) NE
+c) NW
+
+28. Care este inaltimea minima a unui obstacol pentru a fi reprezentat pe o harta de navigatie
+a) 50m
+b) 100m
+c) 300m
+
+29. Inaltimea unui punct este distanta dintre acel punct si
+a) nivelul mediu al marii
+b) izobara de 1013mb
+c) terenul survolat
+
+30. Care sunt coordonatele geografice
+a) latitudine
+b) altitudine
+c) longitudine
+
+31. Cate secunde are 1 grad de latitudine sau longitudine
+a) 60
+b) 3600
+c) 600
+
+32. O mila nautica (1NM) este echivalenta cu
+a) 1 minut de longitudine in orice punct de pe pamant
+b) 1 minut de latitudine in orice punct de pe pamant
+c) 1 grad de latitudine in orice punct de pe pamant
+
+33. Cati kilometri are 1 mila nautica (NM)
+a) 1.8
+b) 1.5
+c) 2.2
+
+34. Cate minute au 0.25&deg;
+a) 45 minute
+b) 15 minute
+c) 25 minute
+
+35. Ce este o zona interzisa
+a) un spatiu definit unde zborul nu este permis
+b) un spatiu definit unde zborul se face dupa reguli stricte
+c) un spatiu definit ca fiind temporar restrictionat
+
+36.  Ce este o zona restrictionata
+a) un spatiu definit unde este restrictionat zborul
+b) un spatiu definit unde zborul se face in conditii specificate
+c) un spatiu definit unde au loc activiati periculoase zborului
+
+37. Deplasarea aeronavei fata de sol este compusa din
+a) viteza aeronavei fata de aer
+b) viteza aeronavei fata de vant
+c) viteza aerului fata de sol
+
+38. Viteza vantului se descompune in
+a) componenta verticala
+b) componenta laterala
+c) componenta longitudinala
+
+39. Ce este deriva
+a) unghiul dintre directia vantului si directia de deplasare fata de sol
+b) unghiul dintre directia de orientare si directia de deplasare fata de aer
+c) unghiul dintre directia de orientare si directia de deplasare fata de sol
+
+40. Acul busolei se va alinia cu Nordul
+a) adevarat
+b) magnetic
+c) izobaric
+
+41. FL50 observat pe o harta reprezinta cu aproximatie
+a) 5000m
+b) 5000ft
+c) 500m
+
+42. In conditii de vant calm directia de deplasare a aeronavei este
+a) perpendiculara pe directia vantului
+b) paralela cu directia vantului
+c) neinfluentata de vant
+
+43. Cu vant din spate viteza aeronavei fata de aer este
+a) mai mare
+b) mai mica
+c) nu difera
+
+44. Cum afiseaza sistemul GPS coordonatele geografice
+a) grade, minute, secunde
+b) grade, minute
+c) grade, secunde
+
+
+45. Hartile trebuie sa fie
+a) digitale
+b) cu scara constanta
+c) conforme (sa respecte unghiurile reale)
+
+46. Scara unei harti poate fi reprezentata ca
+a) fractie reprezentativa (1:500.000)
+b) linie gradata
+c) text descriptiv (1cm egal 5km)
+
+47. Ce ii trebuie unui sistem GPS pentru determinarea pozitiei
+a) cer senin
+b) spatiu neacoperit
+c) conexiune la Internet
+
+48. O viteza a vantului de 20 de noduri (20kts) reprezinta
+a) 15m/s
+b) 10km/h
+c) 10m/s
+
+49. Vantul este din sector sud vestic (SW). Va asteptati ca directia sa fie din
+a) 000&deg; - 045&deg;
+b) 225&deg; - 315&deg;
+c) 180&deg; - 270&deg;
+
+50. Linia care uneste pe o harta punctele cu aceeasi altitudine se numeste
+a) ortodroma
+b) curba de nivel
+c) izoclina
+
+51. Cota este:
+a)  înălţimea obstacolului măsurată faţă de nivelul mediu al mării;
+b) înălţimea obstacolului fata de un nivel de referinta.
+c) distanţa obstacolului măsurată faţă de nivelul mării;
+";
+
+
+$str = iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $str);
+
+
+$arr = explode(PHP_EOL,$str);
+$arrInsert = array();
+foreach($arr as $row) {
+    if(empty($row)) continue;
+    $arrRow = explode(".",$row);
+    if(is_numeric($arrRow[0])) {
+        if(!empty($arrQuestions))
+            $arrInsert[] = $arrQuestions;
+        //limita intre intrebari
+        $arrQuestions = array("category"=>"Navigatie");
+
+        $j=0;
+        $arrQuestions["category_index"] = trim($arrRow[0]);
+        unset($arrRow[0]);
+        $arrQuestions["text"] = implode(".",$arrRow);
+
+    } else {
+        $arrRow2 = explode(")",$row);
+        if(in_array(trim($arrRow2[0]),array("a","b","c"))) {
+            $j++;
+            unset($arrRow2[0]);
+            $arrQuestions["answer_$j"] = implode(")",$arrRow2);
+        } else {
+            $j++;
+            unset($arrRow[0]);
+            $arrQuestions["answer_$j"] = implode(".",$arrRow);
+        }
+    }
+
+
+}
+if(!empty($arrQuestions))
+    $arrInsert[] = $arrQuestions;
+
+
+//echo "<pre>"; var_dump($arrInsert); exit;
+
+$handler = mysqli_connect("localhost");
+mysqli_select_db($handler,"test_repo");
+
+
+$id = 300;
+foreach($arrInsert as $data) {
+    $id++;
+    $query = "INSERT INTO questions SET id=$id,";
+    $values = array();
+
+    foreach($data as $field=>$value) {
+        $values[] = "$field = '$value'";
+    }
+
+    $query .= implode(",",$values);
+    mysqli_query($handler,$query);
+    echo $query."<br />";
+}
