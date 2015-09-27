3c035c30cf9babbc9d17bab116b064225c7d57ac
Dan Dumitriu <dan.dumitriu@sfappworks.com>
2015-09-27 20:27:39 +0300

    import script prim ajutor

---
+++ public/import_prim_ajutor.php
@@ -0,0 +1,196 @@
+<?php
+$str = "
+1. Cu ce se recomanda transportarea persoanelor accidentate, care prezinta fracturi de coloana?
+a) cu un autoturism, deoarece asigura ranitului confort
+b) cu orice mijloc de transport
+c) cu un mijloc de transport cu platforma
+2. Care este prima masura pe care trebuie sa o luati cand o persoana, ce a fost implicata intr-un accident, are o rana cu hemoragie?
+a) sa pansati rana
+b) sa opriti hemoragia
+c) sa curatati rana
+      3. Cum se face oprirea hemoragiei ?
+      a) prin legarea stransa a bratului deasupra rani
+      b) prin legarea lejera a bratului deasupra ranii
+            c) prin legarea stransa a bratului in dreptul ranii
+4. Ce trebuie sa faceti, in primul rand, pentru a salva o persoana implicata intr-un accident?
+a) sa-i eliberati si sa-i degajati caile respiratorii
+b) sa-i indreptati membrele fracturate
+c) sa-i curatati ranile
+5. Cum trebuie asezat un ranit care prezinta leziuni ale coloanei vertebrale?
+a) in aceasta situatie este recomandabil sa nu fie miscat pana la sosirea ambulantei
+b) in pozitie sezanda
+c) orizontal, cu fata in sus
+6. Ce trebuie sa aveti in vedere inaintea transportarii unei victime?
+a) ca autovehiculul cu care o veti transporta sa fie confortabil
+b) ca functiile respiratorii si circulatorii sa fie asigurate
+c) ca victima sa fie asistata, in timpul transportului, de o persoana competenta
+7. Pentru ce se folosesc substantele antiseptice?
+a) pentru oprirea hemoragiei
+b) pentru dezinfectia plagilor
+c) pentru calmarea durerilor
+8. Ce se aplica pe plaga, intr-un pansament?
+a) o compresa oarecare
+b) o compresa de vata sterila
+c) o compresa de tifon steril
+9. Cum trebuie asezata o victima care a pierdut mult sange?
+a) in pozitie sezanda
+b) culcata pe o parte
+c) cu picioarele situate mai sus decat nivelul corpului
+10. Ce se impune pentru tratamentul de urgenta al unor plagi?
+a) curatarea, dezinfectia si pansarea plagilor
+b) punerea tijelor de lemn pe zona ranita
+c) pansarea plagilor
+11. Ce trebuie sa faceti pentru a evita complicatiile in cazul unei fracturi?
+a) sa curatati si sa dezinfectati zona
+b) sa pansati zona fracturata
+c) sa imobilizati zona fracturata
+
+      12. Cum se apreciaza gravitatea unei hemoragii?
+a) in functie de culoarea sangelui pierdut
+b) in functie de fluiditatea sangelui pierdut
+c) in functie de cantitatea sangelui pierdut
+13. Cand se transporta persoanele accidentate ce prezinta hemoragii externe?
+a) imediat dupa oprirea hemoragiei
+b) imediat dupa ce o persoana calificata i-a acordat asistenta medicala
+c) imediat dupa sosirea unui mijloc de transport dotat cu platforma
+14. Cum se pot opri hemoragiile la trunchi si la cap?
+a) prin aplicarea compreselor sterile si a pansamentului compresiv
+b) prin aplicarea garoului
+c) prin aplicarea atelelor
+15. Cu ce se dezinfecteaza plagile inainte de a fi pansate?
+a) acid sulfuric
+b) cu apa oxigenata
+c) cu iod
+16. Unde se aplica garoul in cazul plagilor insotite de hemoragii arteriale?
+a) peste plaga
+b) deasupra plagii
+c) sub plaga
+17. Cum trebuie asezata o persoana ranita care prezinta leziuni ale coloanei vertebrale?
+a) orizontal, cu fata in sus
+ 	      b) in pozitie sezanda
+c) in situatia aceasta nu trebuie sa fie miscat pana la sosirea salvarii
+18. Ce se scrie pe biletul care se prinde langa garoul aplicat unei persoane ranite?
+a) ora si minutul aplicarii garoului
+b) datele de identificare ale victimei
+c) pozitia in care a fost gasita victima
+19. Cum trebuie sa stea o persoana, ce a suferit leziuni intr-un accident, pana la transportarea la spital?
+a) intr-o pozitie cat mai comoda
+b) obligatoriu cu fata in sus
+c) intr-o pozitie care sa permita o buna respiratie
+20. Cum se poate face respiratia artificiala in cazul in care gura ranitului a ramas inclestata?
+a) prin narile victimei
+b) in cazul acesta nu se face respiratie artificiala
+c) in acest caz se face o incizie in obrazul victimei, pe unde apoi se face respiratia artificiala
+21. Cum trebuie sa fie atelele pentru imobilizarea unei fracturi?
+a) sa nu fie foarte grele
+ b) foarte flexibile
+ c) sa fie suficient de lungi pentru a acoperi in intregime atat zona de deasupra, cat si cea de sub fractura
+22. De ce se aplica un bandaj de tifon pe o rana deschisa?
+a) pentru a opri hemoragia
+b) pentru calmarea durerilor
+c) pentru a impiedica infectarea ranii
+23. Ce simptome prezinta o victima a unui accident care a suferit stop cardiac?
+a) aparitia hemoragiei pe gura
+b) lipsa pulsului
+c) innegrirea fetei si inrosirea ochilor
+24. Care poate fi cauza stopului respirator in cazul persoanelor ce au fost implicate intr-un accident?
+a) pierderea unei cantitati mari de sange
+b) o leziune puternica in zona lombara
+c) blocarea cailor respiratorii
+25. Cum sa executati masajul cardiac unei persoane care a fost implicata intr-un accident , in urma caruia i s-au oprit bataile inimii?
+a) prin lovirea usoara cu palmele a obrajilor victimei
+b) prin apasarea ritmica cu podul palmelor suprapuse a toracelui victimei, in dreptul inimii
+c) numai o persoana calificata trebuie sa faca masajul cardiac
+26. Cum se face imobilizarea membrului inferior ce a fost fracturat intr-un accident?
+a) cu ajutorul atelelor
+b) legand strans garoul in jurul membrului
+c) prin bandajarea locului fracturii
+27. Cat timp poate fi mentinut aplicat un garou unei persoane ranite in urma unui accident, fara a-i leza tesuturile?
+a) cel mult 12 ore
+b) cel mult 2 ore
+c) cel putin 2 ore
+
+28. Cum va fi ridicata o persoana grav ranita intr-un accident?
+a) cu atentie, de maini si de picioare de catre doua persoane
+b) cu atentie, mentinandu-se in acelasi plan capul, gatul si toracele
+c) mentinand picioarele in sus si capul ceva mai jos decat trunchiul, pentru o mai buna circulatie a sangelui in organism
+29. Cum se va opri o hemoragie produsa la unul din membrele unei persoane ranite?
+a) prin aplicarea garoului
+b) prin actiunea compresiva asupra vasului lezat
+c) prin apasarea ritmica a plagii cu podul palmelor suprapuse
+30. Caile respiratorii se pot astupa din cauza ca:
+a) victima a suferit un atac de panica
+b) victimei i-au intrat corpuri straine pe caile respiratorii
+c)  victima sufera de scolioza
+      31. Ce este o entorsa?
+a) leziune inchisa a unei articulatii, care a fost fortata sa faca o miscare / deplasare exagerata, dupa care suprafetele articulare ale oaselor si-au revenit la pozitia normala
+b) leziune a unei articulatii, la care suprafetele articulare ale oaselor sunt deplasate permanent din pozitia normala
+c) intreruperea totala sau partiala a continuitatii unui os
+      32. Ce este  luxatia?
+a) leziune inchisa a unei articulatii, care a fost fortata sa faca o miscare / deplasare exagerata, dupa care suprafetele articulare ale oaselor si-au revenit la pozitia normala
+b) leziune a unei articulatii, la care suprafetele articulare ale oaselor sunt deplasate permanent din pozitia normala
+c) intreruperea totala sau partiala a continuitatii unui os
+";
+
+
+$str = iconv('UTF-8', 'ISO-8859-1//TRANSLIT//IGNORE', $str);
+
+
+$arr = explode(PHP_EOL,$str);
+$arrInsert = array();
+foreach($arr as $row) {
+    if(empty($row)) continue;
+    $arrRow = explode(".",$row);
+    if(is_numeric($arrRow[0])) {
+        if(!empty($arrQuestions))
+            $arrInsert[] = $arrQuestions;
+        //limita intre intrebari
+        $arrQuestions = array("category"=>"Prim ajutor");
+
+        $j=0;
+        $arrQuestions["category_index"] = trim($arrRow[0]);
+        unset($arrRow[0]);
+        $arrQuestions["text"] = implode(".",$arrRow);
+
+    } else {
+        $arrRow2 = explode(")",$row);
+        if(in_array(trim($arrRow2[0]),array("a","b","c"))) {
+            $j++;
+            unset($arrRow2[0]);
+            $arrQuestions["answer_$j"] = implode(")",$arrRow2);
+        } else {
+            $j++;
+            unset($arrRow[0]);
+            $arrQuestions["answer_$j"] = implode(".",$arrRow);
+        }
+    }
+
+
+}
+if(!empty($arrQuestions))
+    $arrInsert[] = $arrQuestions;
+
+
+//echo "<pre>"; var_dump($arrInsert); exit;
+
+$handler = mysqli_connect("localhost");
+mysqli_select_db($handler,"test_repo");
+
+
+$id = 800;
+foreach($arrInsert as $data) {
+    $id++;
+    $query = "INSERT INTO questions SET id=$id,";
+    $values = array();
+
+    foreach($data as $field=>$value) {
+        $values[] = "$field = '$value'";
+    }
+
+    $query .= implode(",",$values);
+    mysqli_query($handler,$query);
+    echo $query."<br />";
+}
+
+?>
+
